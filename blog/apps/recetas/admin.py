e666108f13081c1c17f4b74f8006d1a0dd7eb447
pelado-o <146124369+pelado-o@users.noreply.github.com>
2023-12-27 00:50:38 -0300

    Add files via upload

--- blog/apps/recetas/admin.py
+++ blog/apps/recetas/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Categoria, Post
 
 # Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'titulo','subtitulo','fecha','texto','activo','categoria','imagen','publicado')
+
+admin.site.register(Categoria)
--- blog/apps/recetas/models.py
+++ blog/apps/recetas/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
+
+#CATEGORIA
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=30, null=False)
+
+    def __str__(self):
+        return self.nombre
+
+class Post(models.Model):
+    titulo = models.CharField(max_length=50, null=False)
+    subtitulo = models.CharField(max_length=100, null=True, blank=True)
+    fecha = models.DateTimeField(auto_now_add=True)
+    texto = models.TextField(null=False)
+    activo = models.BooleanField(default=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True, default='Sin categoría')
+    imagen = models.ImageField(null=True, blank=True, upload_to='media', default='static/post_default.png')
+    publicado = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        ordering = ('-publicado',)
+
+    def __str__(self):
+        return self.titulo
+        
+    def delete(self, using =None, keep_parents=False):
+        self.imagen.delete(self.imagen.name)
+        super().delete()
+            
